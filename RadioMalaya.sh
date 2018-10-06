25779048fd73d7bbf4ef24b4f9cb21cc131e1e6e
Joel Gerber <joel@grrbrr.ca>
2018-10-06 16:50:06 -0400

    Fix 'shbang' line
    Without proper formatting, you have to invoke the script as
    sh ./RadioMalaya.sh instead of being able to simply run
    ./RadioMalaya.sh

--- RadioMalaya.sh
+++ RadioMalaya.sh
@@ -1,4 +1,5 @@
-# ! /bin/sh
+#!/bin/sh
+
 #  About : A total bash script for listening local radio stream using mplayer
 #  created by Robbi Nespu < email : robbi . nespu @ hotmail .com >
 
@@ -6,12 +7,12 @@
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
